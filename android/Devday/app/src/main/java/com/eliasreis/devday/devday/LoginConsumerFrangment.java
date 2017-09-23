c4078a48d5483de7d99183782818847738c88381
eliasreis54 <eliasreis54@gmail.com>
2017-09-23 19:49:19 +0000

    Projeto Final

--- android/Devday/app/src/main/java/com/eliasreis/devday/devday/LoginConsumerFrangment.java
+++ android/Devday/app/src/main/java/com/eliasreis/devday/devday/LoginConsumerFrangment.java
@@ -1,10 +1,14 @@
 package com.eliasreis.devday.devday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginConsumerFrangment extends Fragment {
 
@@ -18,6 +22,20 @@ public class LoginConsumerFrangment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login_consumer_frangment, container, false);
+        View view = inflater.inflate(R.layout.fragment_login_consumer_frangment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.login_button)
+    public void login() {
+        Intent intent = new Intent(getActivity(), SigInConsumerActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.text_login)
+    public void loginTxt(){
+        Intent intent = new Intent(getActivity(), SigInConsumerActivity.class);
+        startActivity(intent);
     }
 }
---
+++ android/Devday/app/src/main/java/com/eliasreis/devday/devday/SigInConsumerActivity.java
@@ -0,0 +1,13 @@
+package com.eliasreis.devday.devday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigInConsumerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sig_in_consumer);
+    }
+}
